b3f9c26702349960455907e64a8fa93d67187df8
KVBAMusic <kubaokon8@gmail.com>
2024-10-24 09:12:49 +0200

    Cw2_1_1

--- script.js
+++ script.js
@@ -13,9 +13,33 @@
         answer.innerHTML = JSON.stringify(array);
       })
   })
-
+  
   cw1.addEventListener("click", function () {
-    //TODO
+    answer.innerText = "Loading..."
+    fetch(`https://jsonplaceholder.typicode.com/posts/`)
+      .then(response => response.json())
+      .then(array => {
+        id++;
+        answer.innerHTML = "";
+        console.log(array)
+        for (let post of array) {
+          const elem = document.createElement("div")
+          const title = document.createElement("h3");
+          const user = document.createElement("p");
+          const postid = document.createElement("p");
+          const text = document.createElement("p");
+          title.innerText = post.title;
+          user.innerText = `User ID: ${post.userId}`;
+          text.innerText = post.body;
+          postid.innerText = post.id;
+
+          elem.appendChild(postid);
+          elem.appendChild(title);
+          elem.appendChild(user);
+          elem.appendChild(text);
+          answer.appendChild(elem);
+        }
+      })
   })
 
   cw2.addEventListener("click", function () {
