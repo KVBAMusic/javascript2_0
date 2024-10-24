455e3cdd1fdc7e6977e6e187c36469d5cf9fbaf1
KVBAMusic <kubaokon8@gmail.com>
2024-10-24 09:41:57 +0200

    Cw2_2

--- script.js
+++ script.js
@@ -15,11 +15,16 @@
   })
 
   const createPost = (post) => {
-    const elem = document.createElement("div")
-    const title = document.createElement("h3");
+    const elem = document.createElement("div");
+    const title = document.createElement("h2");
     const user = document.createElement("p");
     const postid = document.createElement("p");
     const text = document.createElement("p");
+
+    elem.classList.add("post")
+    title.classList.add("post-title")
+    text.classList.add("post-body");
+
     title.innerText = post.title;
     user.innerText = `User ID: ${post.userId}`;
     text.innerText = post.body;
@@ -29,7 +34,6 @@
     elem.appendChild(title);
     elem.appendChild(user);
     elem.appendChild(text);
-    elem.appendChild(document.createElement("hr"))
     return elem;
   };
 
@@ -67,7 +71,16 @@
   })
 
   cw2.addEventListener("click", function () {
-    //TODO
+    alert("Loading...");
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        answer.innerHTML = "";
+        for (let post of array) {
+          answer.append(createPost(post));
+          console.log(`id: ${post.id}\tuserId: ${post.userId}\ttitle: ${post.title}\tbody: ${post.body}`);
+        }
+      })
   })
 
   cw3.addEventListener("click", function () {
